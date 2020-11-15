037812efd3f59b606965a92a4cc57c4d85d48635
Daniel Borges <dbborges.rj@gmail.com>
2020-11-15 11:22:32 -0300

    Criando mapeamento das classes de dominio

---
+++ src/main/java/com/danielborges/bookstoremanager/books/entity/Book.java
@@ -0,0 +1,39 @@
+package com.danielborges.bookstoremanager.books.entity;
+
+import com.danielborges.bookstoremanager.authors.entity.Author;
+import com.danielborges.bookstoremanager.publishers.entity.Publisher;
+import com.danielborges.bookstoremanager.users.entity.User;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(columnDefinition = "integer default 0")
+    private int pages;
+
+    @Column(columnDefinition = "integer default 0")
+    private int chapters;
+
+    @ManyToOne(cascade = {CascadeType.MERGE})
+    private Author author;
+
+    @ManyToOne(cascade = {CascadeType.MERGE})
+    private Publisher publisher;
+
+    @ManyToOne(cascade = {CascadeType.MERGE})
+    private User user;
+
+}
