84dac39a88f330ea41701305c5cf5a40a1ea1c0c
Daniel Borges <dbborges.rj@gmail.com>
2020-11-21 20:20:53 -0300

    Developed authors exclusions by id

--- src/main/java/com/danielborges/bookstoremanager/authors/controller/AuthorControllerDocs.java
+++ src/main/java/com/danielborges/bookstoremanager/authors/controller/AuthorControllerDocs.java
@@ -33,4 +33,11 @@ public interface AuthorControllerDocs {
             @ApiResponse(code = 200, message = "Return all registered authors")
     })
     List<AuthorDTO> findAll();
+
+    @ApiOperation(value = "Delete author by id operation")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "Success author deleted"),
+            @ApiResponse(code = 404, message = "Author not found error code")
+    })
+    void delete(@PathVariable Long id);
 }
--- src/test/java/com/danielborges/bookstoremanager/author/controller/AuthorControllerTest.java
+++ src/test/java/com/danielborges/bookstoremanager/author/controller/AuthorControllerTest.java
@@ -18,14 +18,19 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.StatusResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import javax.management.RuntimeErrorException;
+import javax.print.attribute.standard.Media;
 
 import java.util.Collections;
 
@@ -113,4 +118,15 @@ public class AuthorControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].age", is(expectedFoundAuthorDTO.getAge())));
     }
 
+    @Test
+    void whenDELETEWithValidIdIsCalledThenNoContentShouldBeReturned() throws Exception {
+        AuthorDTO expectedAuthorDeletedDTO = authorDTOBuilder.buildAuthorDTO();
+
+        var expectedAuthorDeletedId = expectedAuthorDeletedDTO.getId();
+        doNothing().when(authorService).delete(expectedAuthorDeletedId);
+
+        mockMvc.perform(delete(AUTHOR_API_URL_PATH + "/" + expectedAuthorDeletedId)
+        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
 }
