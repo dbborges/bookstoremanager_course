6da23af82fe8584075c861ce5a2cb9666528eb6d
Daniel Borges <dbborges.rj@gmail.com>
2020-11-22 14:42:00 -0300

    Created User DTO class

---
+++ src/main/java/com/danielborges/bookstoremanager/users/dto/UserDTO.java
@@ -0,0 +1,51 @@
+package com.danielborges.bookstoremanager.users.dto;
+
+import com.danielborges.bookstoremanager.users.enums.Gender;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Long id;
+
+    @NotNull
+    @NotEmpty
+    @Size(max = 255)
+    private String name;
+
+    @NotNull
+    @Max(120)
+    private int age;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Gender gender;
+
+    @NotNull
+    @NotEmpty
+    @Email
+    private String email;
+
+    @NotNull
+    @NotEmpty
+    private String username;
+
+    @NotNull
+    @NotEmpty
+    private String senha;
+
+    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate birthDate;
+}
