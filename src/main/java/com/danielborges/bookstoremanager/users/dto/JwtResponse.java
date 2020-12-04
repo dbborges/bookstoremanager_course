882cb37ecc18866901b079ca5ea5003ac1180b75
Daniel Borges <dbborges.rj@gmail.com>
2020-12-04 13:50:20 -0300

    Created jwt request and response classes

---
+++ src/main/java/com/danielborges/bookstoremanager/users/dto/JwtResponse.java
@@ -0,0 +1,12 @@
+package com.danielborges.bookstoremanager.users.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class JwtResponse {
+
+    private final String jwtToken;
+
+}
